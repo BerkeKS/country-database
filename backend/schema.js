97a3183ba481121a942df0d90ad1d1a27ba5dafd
BerkeKS <berkekasabah@gmail.com>
2024-09-19 23:35:42 +0300

    Add model of country for database

---
+++ backend/schema.js
@@ -0,0 +1,34 @@
+const { default: mongoose } = require("mongoose")
+const { soccerLeagues } = require("./map")
+
+const schema = require("mongoose").Schema
+
+const countrySchema = new schema ({
+    name: {
+    },
+
+    intro: {
+        type: String
+    },
+
+    timezone: {
+        type: String,
+    },
+
+    images: {
+        type: Array,
+        required: true
+    },
+
+    attractions: {
+        type : Array,
+        default : []
+    },
+
+    soccerLeagues: {
+        type: Array,
+    }
+})
+
+const country = mongoose.model("Country", countrySchema)
+module.exports = country
