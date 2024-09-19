da939c506f69e1d7817dbc14320f90e636a1129c
BerkeKS <berkekasabah@gmail.com>
2024-09-19 23:36:41 +0300

    Construct main link for routers

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,12 @@ const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const conn = require('./db')();
 const cors = require('cors');
+const router = require('./router');
 
 app.use(express.json());
 app.use(cors()); //Cors for middleware - To avoid blocks
+app.use('/', router);
 
 app.listen(PORT, () => {
-    console.log('API is active on, ', PORT);
+    console.log('API is active on ', PORT);
 })
