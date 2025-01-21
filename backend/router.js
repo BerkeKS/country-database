74f324a1d27b3313eff4ce8b6f9289eb410f5b95
BerkeKS <berkekasabah@gmail.com>
2025-01-21 13:28:35 +0300

    Change endpoint of geCountry function

--- backend/router.js
+++ backend/router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const controller = require('./controller')
 
-router.get("/get", controller.getCountry)
+router.get("/getCountry", controller.getCountry)
 
 module.exports = router
