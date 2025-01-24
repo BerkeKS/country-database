6f6a54163ff671eb18a7d1593e1bc59752e4c4e2
BerkeKS <berkekasabah@gmail.com>
2025-01-24 21:50:29 +0300

    Add router link for createAttraction method

--- backend/router.js
+++ backend/router.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const controller = require('./controller')
 
 router.get("/getcountry", controller.getCountry)
+router.post("/createattraction", controller.createAttraction)
 router.get("/addattraction/:country_id", controller.addAttraction)
 
 module.exports = router
